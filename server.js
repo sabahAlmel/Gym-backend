15c93eed80d658ef3d3402bd07659c3d6d220558
souhad <souhadmoussa86@gmail.com>
2023-10-20 16:13:00 +0300

    fixed server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 import express from "express";
-import dbConnect from "./src/config/dbConnection.js";
-
 
 const app = express()
 
 app.listen(5000, () => {
     console.log(`Listening on port 5000`)
 })
-dbConnect()
+
