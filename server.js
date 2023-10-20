e63d8be34f3fcb481470c6b66e918165930a62b9
souhad <souhadmoussa86@gmail.com>
2023-10-20 15:40:55 +0300

    connected to database/ dbConnection.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 import express from "express";
+import dbConnect from "./src/config/dbConnection.js";
+
 
 const app = express()
 
-app.listen(5000, ()=>{
+app.listen(5000, () => {
     console.log(`Listening on port 5000`)
 })
+dbConnect()
--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,2 +1,32 @@
+
+
+
+import "dotenv/config"
 import mongoose from "mongoose";
+
+async function dbConnect() {
+    try {
+        await mongoose.connect(process.env.URL)
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+
+
+
+
+
+
+//database connection 
+// const url = process.env.URL
+
+// console.log(url)
+
+// async function dbConnect() {
+//     await mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true });
+// }
+
+
+export default dbConnect;
 
