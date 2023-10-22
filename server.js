8d254ed1d681bdd36a5f57864036b3035a628ffb
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2023-10-22 22:18:26 +0300

    Constructing Product's schema and its MVC

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-import express from "express";
+import express from 'express'
+import dbconnect from './src/config/dbConnection'
+import router from './src/routes/productRoutes'
 
 const app = express()
+
+dbconnect()
+
+app.use(express.json())
+app.use('/dashboard/product', router)
 
 app.listen(5000, ()=>{
     console.log(`Listening on port 5000`)
--- src/config/dbConnection.js
+++ src/config/dbConnection.js
@@ -1,2 +1,17 @@
-import mongoose from "mongoose";
+import mongoose from "mongoose"
+import 'dotenv/config'
 
+async function dbconnect() {
+    try {
+        await mongoose.connect(process.env.MONG_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log("Connected to MongoDB")
+    }
+    catch (error) {
+        console.log("Error conntecting to Database: ", error)
+    }
+}
+
+export default dbconnect
