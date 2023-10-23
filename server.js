ed72db850b81a989db471e03206638d408470747
Hasan Deeb <sektories7@gmail.com>
2023-10-23 21:41:51 +0300

    fiixed errors

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 import express, { urlencoded } from "express";
 import dbConnect from "./src/config/dbConnection.js";
 // import trainingRouter from "./src/routes/trainingRouter.js";
@@ -7,29 +6,22 @@ import productsRouter from "./src/routes/productsRouter.js";
 // import socialsRouter from "./src/routes/socialsRouter.js";
 // import gymPlansRouter from "./src/routes/gymplansRouter.js";
 
-import 'dotenv/config'
+import "dotenv/config";
 
-import express from "express";
+const app = express();
+const port = process.env.PORT
+dbConnect();
 
-const app = express()
-
-
-dbconnect()
-
-app.use(express.json())
-
-
-
+app.use(express.json());
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`);
+});
 
-})
-
-app.use(express.json()); 
-app.use(urlencoded({extended: true}))
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
 // app.use('/training',trainingRouter)
-app.use('/regime', regimeRouter)
-app.use('/products',productsRouter)
+app.use("/regime", regimeRouter);
+app.use("/products", productsRouter);
 // app.use('/socials',socialsRouter)
 // app.use('/gymPlans',gymPlansRouter)
--- src/models/productModel.js
+++ src/models/productModel.js
@@ -1,28 +1,22 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-
-
-const {Schema , model} =mongoose
+const { Schema, model } = mongoose;
 
 const prodSchema = new Schema({
-        prodName: {
-            type: String,
-            required: true
-        },
-        prodPrice: {
-            type: Number,
-            required: true
-        },
-        prodImage: {
-            type: String,
-            type: Blob,
-            required: true
-        }
-    }
-)
+  prodName: {
+    type: String,
+    required: true,
+  },
+  prodPrice: {
+    type: Number,
+    required: true,
+  },
+  prodImage: {
+    type: String,
+    required: true,
+  },
+});
 
+const prod = model("Product", prodSchema);
 
- const prod = model("Product", prodSchema)
-
-export default model('Product', prodSchema)
-
+export default model("Product", prodSchema);
