407437113bcb4aca7b8aa4ecd4d25f242a5ba2fa
Hasan Deeb <sektories7@gmail.com>
2023-10-23 20:49:55 +0300

    regime plans CRUD

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 import express, { urlencoded } from "express";
 import dbConnect from "./src/config/dbConnection.js";
-import trainingRouter from "./src/routes/trainingRouter.js";
+// import trainingRouter from "./src/routes/trainingRouter.js";
 import regimeRouter from "./src/routes/regimeRouter.js";
-import productsRouter from "./src/routes/productsRouter.js";
-import socialsRouter from "./src/routes/socialsRouter.js";
-import gymPlansRouter from "./src/routes/gymplansRouter.js";
+// import productsRouter from "./src/routes/productsRouter.js";
+// import socialsRouter from "./src/routes/socialsRouter.js";
+// import gymPlansRouter from "./src/routes/gymplansRouter.js";
+// import multer from "multer";
+// import {gridFsStorage} from 
 import 'dotenv/config'
 const app = express()
 dbConnect()
@@ -16,8 +18,8 @@ app.listen(port, () => {
 
 app.use(express.json()); 
 app.use(urlencoded({extended: true}))
-app.use('/training',trainingRouter)
+// app.use('/training',trainingRouter)
 app.use('/regime', regimeRouter)
-app.use('/products',productsRouter)
-app.use('/socials',socialsRouter)
-app.use('/gymPlans',gymPlansRouter)
+// app.use('/products',productsRouter)
+// app.use('/socials',socialsRouter)
+// app.use('/gymPlans',gymPlansRouter)
