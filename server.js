ce2d4010517f4bc3acff927d103fc26400d027f5
Hasan Deeb <sektories7@gmail.com>
2023-10-24 21:05:34 +0300

    regime and training plans

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ import dbConnect from "./src/config/dbConnection.js";
 import trainingRouter from "./src/routes/trainingRouter.js";
 import regimeRouter from "./src/routes/regimeRouter.js";
 import productsRouter from "./src/routes/productsRouter.js";
-// import socialsRouter from "./src/routes/socialsRouter.js";
-// import gymPlansRouter from "./src/routes/gymplansRouter.js";
+import socialsRouter from "./src/routes/socialsRouter.js";
+import gymPlansRouter from "./src/routes/gymplansRouter.js";
 
 import 'dotenv/config'
 
@@ -15,7 +15,8 @@ const app = express()
 
 dbConnect()
 
-app.use(express.json())
+app.use(express.json()); 
+
 
 const port = process.env.PORT
 
@@ -25,10 +26,9 @@ app.listen(port, () => {
 
 })
 
-app.use(express.json()); 
 app.use(urlencoded({extended: true}))
 app.use('/training',trainingRouter)
 app.use('/regime', regimeRouter)
 app.use('/products',productsRouter)
-// app.use('/socials',socialsRouter)
-// app.use('/gymPlans',gymPlansRouter)
+app.use('/socials',socialsRouter)
+app.use('/gymPlans',gymPlansRouter)
--- src/routes/trainingRouter.js
+++ src/routes/trainingRouter.js
@@ -1,10 +1,18 @@
-import express from 'express'
-import { addTrainingService, deleteTraining, getTrainingServices } from '../controllers/trainingController.js'
+import express from "express";
+import {
+  addTrainingService,
+  deleteTraining,
+  getTrainingServices,
+  updateTraining,
+} from "../controllers/trainingController.js";
+import { checkReqId } from "../middlewares/CheckIdMiddleware.js";
+import upload from "../middlewares/upload.js";
 
-const trainingRouter = express.Router()
+const trainingRouter = express.Router();
 
-trainingRouter.get('/get', getTrainingServices)
-trainingRouter.post('/add', addTrainingService)
-trainingRouter.delete('/delete', deleteTraining)
+trainingRouter.get("/get", getTrainingServices);
+trainingRouter.post("/add", upload.single("trainingImg"), addTrainingService);
+trainingRouter.delete("/delete", checkReqId, deleteTraining);
+trainingRouter.patch("/update", upload.single("trainingImg"), updateTraining);
 
 export default trainingRouter;
