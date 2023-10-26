9e98b6456bc764c3f88879e61455d44277d39c04
Hasan Deeb <sektories7@gmail.com>
2023-10-26 09:30:38 +0300

    fixed express static

--- server.js
+++ server.js
@@ -10,23 +10,21 @@ import gymPlansRouter from "./src/routes/gymPlansRouter.js";
 import "dotenv/config";
 
 const app = express();
-const port = process.env.PORT
+const port = process.env.PORT;
 dbConnect();
 
 app.use(express.json());
-
-
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-app.use(express.static('images'))
+app.use("/images", express.static("images"));
 
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
-app.use('/training',trainingRouter)
+app.use("/training", trainingRouter);
 app.use("/regime", regimeRouter);
 app.use("/products", productsRouter);
-app.use('/socials',socialsRouter)
-app.use('/gymPlans',gymPlansRouter)
+app.use("/socials", socialsRouter);
+app.use("/gymPlans", gymPlansRouter);
