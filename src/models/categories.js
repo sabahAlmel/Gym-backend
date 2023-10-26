37f640eabaa30450653a81af48ebdc6b07feb1db
Hasan Deeb <sektories7@gmail.com>
2023-10-26 19:16:30 +0300

    added category

---
+++ src/models/categories.js
@@ -0,0 +1,9 @@
+import { Schema, model } from "mongoose";
+
+const categorySchema = new Schema({
+    name: {type: String, required : true},
+    products: {type: Schema.Types.ObjectId, ref:'Product'},
+    regime: {type: Schema.Types.ObjectId, ref: "Regime Plans"}
+})
+
+export default model('Categories', categorySchema)
