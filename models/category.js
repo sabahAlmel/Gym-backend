59b36fe548f3304198fbc53f2e03516775fbf7cc
abdulaziz <aboudecharkawi@gmail.com>
2023-12-23 10:47:11 +0200

    edit some stuff

--- models/category.js
+++ models/category.js
@@ -10,7 +10,7 @@ export default (sequelize, DataTypes) => {
      */
     static associate(models) {
      category.hasMany(models.productModel, {as:"products", onDelete:"CASCADE"})
-     category.hasMany(models.regimeModel, {onDelete:"CASCADE"})
+    //  category.hasMany(models.regimeModel, {onDelete:"CASCADE"})
     }
   }
   category.init({
